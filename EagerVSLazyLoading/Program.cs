c8bef04e9c8394975071d36670deb63294daff3a
Dang Hoang Khang <dhkhang1709@gmail.com>
2022-03-04 10:59:56 +0700

    Add project files.

---
+++ EagerVSLazyLoading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EagerVSLazyLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
